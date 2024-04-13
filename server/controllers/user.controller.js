428cf64b0ba1cc05e3a646fd0fdaea7f8cbeb525
VJ-108 <vaibhav160205@gmail.com>
2024-04-13 18:42:07 +0530

    added user routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,14 +2,45 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
-const generateAccessandRefreshToken = (userId) => {
+const formatLastSeen = (lastSeen) => {
+  const lastSeenDate = new Date(lastSeen);
+  const now = new Date();
+  const diffMilliseconds = now - lastSeenDate;
+  const diffSeconds = Math.floor(diffMilliseconds / 1000);
+  const diffMinutes = Math.floor(diffSeconds / 60);
+  const diffHours = Math.floor(diffMinutes / 60);
+  const diffDays = Math.floor(diffHours / 24);
+  const diffMonths = Math.floor(diffDays / 30);
+
+  if (diffMonths > 0) {
+    if (diffMonths === 1 && diffDays <= 30) {
+      return "1 month ago";
+    } else if (diffMonths < 6) {
+      return `${diffMonths} month${diffMonths > 1 ? "s" : ""} ago`;
+    } else {
+      return "Long time ago";
+    }
+  } else if (diffDays > 0) {
+    return `${diffDays} day${diffDays > 1 ? "s" : ""} ago`;
+  } else if (diffHours > 0) {
+    return `${diffHours} hour${diffHours > 1 ? "s" : ""} ago`;
+  } else if (diffMinutes > 0) {
+    return `${diffMinutes} minute${diffMinutes > 1 ? "s" : ""} ago`;
+  } else {
+    return "Just Now";
+  }
+};
+
+const generateAccessandRefreshToken = async (userId) => {
   try {
-    const user = User.findById(userId);
+    const user = await User.findById(userId);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
     user.refreshToken = refreshToken;
-    user.save({ validateBeforeSave: false });
+    await user.save({ validateBeforeSave: false });
     return {
       accessToken,
       refreshToken,
@@ -52,4 +83,183 @@ const registerUser = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if ([email, password].some((field) => field?.trim() === "")) {
+      throw new ApiError(400, "All fields are required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new ApiError(401, "User not found");
+    }
+    const isMatch = await user.isPasswordCorrect(password);
+    if (!isMatch) {
+      throw new ApiError(401, "Invalid credentials");
+    }
+    const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+      user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { user: loggedInUser, accessToken, refreshToken },
+          "User logged in successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(500, "Error while logging in user");
+  }
+});
+
+const logOutUser = asyncHandler(async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $unset: {
+          refreshToken: 1,
+        },
+      },
+      { new: true }
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "User logged out successfully"));
+  } catch (error) {
+    throw new ApiError(500, "Error while logging out user");
+  }
+});
+
+const refreshAccessToken = asyncHandler(async (req, res, next) => {
+  try {
+    const oldrefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    if (!oldrefreshToken) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(
+      oldrefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    if (oldrefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh Token is expired or used");
+    }
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+      user._id
+    );
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { user: user, accessToken, refreshToken: refreshToken },
+          "User refreshed successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Refresh Token");
+  }
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    const user = await User.findById(req.user?._id);
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+    if (!isPasswordCorrect) {
+      throw new ApiError(401, "Invalid old password");
+    }
+    user.password = newPassword;
+    user.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Password changed successfully"));
+  } catch (error) {
+    throw new ApiError(500, "Error while changing current password");
+  }
+});
+
+const changeAbout = asyncHandler(async (req, res, next) => {
+  try {
+    const { about } = req.body;
+    const user = await User.findById(req.user?._id);
+    user.about = about;
+    user.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "About changed successfully"));
+  } catch (error) {
+    throw new ApiError(500, "Error while changing about");
+  }
+});
+
+const getLastseen = asyncHandler(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user?._id);
+    const lastSeen = user.lastSeen;
+    const formattedLastSeen = formatLastSeen(lastSeen);
+    return res.json(new ApiResponse(200, `Last seen ${formattedLastSeen}`));
+  } catch (error) {
+    throw new ApiError(500, "Error while fetching lastseen");
+  }
+});
+
+const updateLastseen = asyncHandler(async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      { lastSeen: new Date() },
+      { new: true }
+    );
+    const lastSeen = user.lastSeen;
+    const formattedLastSeen = formatLastSeen(lastSeen);
+    return res.json(new ApiResponse(200, `Last seen ${formattedLastSeen}`));
+  } catch (error) {
+    throw new ApiError(500, "Error while updating lastseen");
+  }
+});
+
+
+export {
+  registerUser,
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  changeAbout,
+  getLastseen,
+  updateLastseen,
+};
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,24 @@
 import Router from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+    changeAbout,
+  getLastseen,
+  updateLastseen,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = new Router();
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/change-about").post(verifyJWT, changeAbout);
+router.route("/lastseen").get(verifyJWT, getLastseen);
+router.route("/update-lastseen").post(verifyJWT, updateLastseen);
 
 export default router;
